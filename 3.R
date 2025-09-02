9e5af59f2df92f28e9a7e32d48162aae7221627a
Snehal-AS <snehal.urs021203@gmail.com>
2025-09-02 10:24:45 +0530

    Update 3.R

--- 3.R
+++ 3.R
@@ -10,4 +10,12 @@ boxplot (df$Barry.Price, df$Mary.Price,
         col = c("skyblue","lightgreen"),
         main = "boxplot of price quotes",
         ylab = "price")
+cat(sd(df$Mary.Price))
+cat(sd(df$Barry.Price))
 
+
+n1 = length(df$Mary.Price)
+n2 = length(df$Barry.Price)
+
+cat((sd(df$Mary.Price))/sqrt(n1))
+cat((sd(df$Barry.Price))/sqrt(n2))
